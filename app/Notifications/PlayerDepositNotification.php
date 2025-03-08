0c84ff7d5007116b39b15d66e8a34fbb85b60bae
AMK1215 <aungmyokhaing1215@gmail.com>
2025-03-08 10:32:17 +0630

    noti

--- app/Notifications/PlayerDepositNotification.php
+++ app/Notifications/PlayerDepositNotification.php
@@ -1,102 +1,36 @@
 <?php
-
 namespace App\Notifications;
 
-use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\BroadcastMessage;
 use Illuminate\Notifications\Notification;
-use App\Events\DepositNotifyEvent;
+use Illuminate\Support\Facades\Log;
 
-class PlayerDepositNotification extends Notification implements ShouldBroadcast, ShouldQueue
+
+class PlayerDepositNotification extends Notification implements ShouldQueue
 {
-    use InteractsWithSockets, Queueable;
+    use Queueable;
 
-    /**
-     * Create a new notification instance.
-     */
-    // protected $deposit;
+    protected $deposit;
 
-    // /**
-    //  * Create a new notification instance.
-    //  */
-    // public function __construct($deposit)
-    // {
-    //     $this->deposit = $deposit;
-    // }
-
-    // /**
-    //  * Determine how the notification will be delivered.
-    //  */
-    // public function via($notifiable)
-    // {
-    //     return ['database', 'broadcast'];
-    // }
-
-    // /**
-    //  * Store notification in the database.
-    //  */
-    // public function toDatabase($notifiable)
-    // {
-    //     return [
-    //         'player_name' => $this->deposit->user->user_name,
-    //         'amount' => $this->deposit->amount,
-    //         'refrence_no' => $this->deposit->refrence_no,
-    //         'message' => "Player {$this->deposit->user->user_name} has deposited {$this->deposit->amount}.",
-    //     ];
-    // }
-
-    // /**
-    //  * Broadcast the notification (for real-time updates).
-    //  */
-    // public function toBroadcast($notifiable)
-    // {
-    //     return new BroadcastMessage([
-    //         'player_name' => $this->deposit->user->user_name,
-    //         'amount' => $this->deposit->amount,
-    //         'refrence_no' => $this->deposit->refrence_no,
-    //         'message' => "Player {$this->deposit->user->user_name} has deposited {$this->deposit->amount}.",
-    //     ]);
-    // }
-
-    // /**
-    //  * Get the broadcast channel.
-    //  */
-    // // public function broadcastOn()
-    // // {
-    // //     return new PrivateChannel('agent.'.$this->deposit->agent_id);
-    // // }
-
-    //  public function broadcastOn()
-    // {
-    //     return new Channel('agent.' . $this->deposit->agent_id);
-    // }
-
-     protected $deposit;
-
-    /**
-     * Create a new notification instance.
-     */
     public function __construct($deposit)
     {
         $this->deposit = $deposit;
     }
 
-    /**
-     * Determine how the notification will be delivered.
-     */
     public function via($notifiable)
     {
         return ['database', 'broadcast'];
     }
 
-    /**
-     * Store notification in the database.
-     */
     public function toDatabase($notifiable)
     {
+         Log::info('Storing notification in database:', [
+        'player_name' => $this->deposit->user->user_name,
+        'amount' => $this->deposit->amount,
+        'refrence_no' => $this->deposit->refrence_no,
+    ]);
         return [
             'player_name' => $this->deposit->user->user_name,
             'amount' => $this->deposit->amount,
@@ -105,14 +39,8 @@ class PlayerDepositNotification extends Notification implements ShouldBroadcast,
         ];
     }
 
-    /**
-     * Broadcast the notification (for real-time updates).
-     */
     public function toBroadcast($notifiable)
     {
-        // Trigger the DepositNotifyEvent for broadcasting
-        event(new DepositNotifyEvent($this->deposit));
-
         return new BroadcastMessage([
             'player_name' => $this->deposit->user->user_name,
             'amount' => $this->deposit->amount,
@@ -120,5 +48,4 @@ class PlayerDepositNotification extends Notification implements ShouldBroadcast,
             'message' => "Player {$this->deposit->user->user_name} has deposited {$this->deposit->amount}.",
         ]);
     }
-
 }
